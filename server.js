6338b64fb27dc4421595e158937df1ae73c968c4
CodyBones101 <iamaplsoz@gmail.com>
2023-02-10 19:09:45 -0500

    log in and nav bar intro

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var methodOverride = require('method-override');
+var passport = require('passport');
+var session = require('express-session');
+
+
 require('dotenv').config();
 require('./config/database');
+require('./config/passport');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,9 +26,19 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(methodOverride('_method'));
-
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
--- your-personal-trainer-reviews/config/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', true);
-mongoose.connect(process.env.DATABASE_URL);
-const db = mongoose.connection;
-
-db.on('connected', function() {
-    console.log(`connected to mongodb ${db.name} at ${db.host} on ${db.port}`)
-})
